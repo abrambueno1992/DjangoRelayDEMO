73617525ea77203a2aba4380d20512b811c898e1
Abraham Bueno <abueno@localhost.localdomain>
2018-09-21 00:21:48 -0500

    Troubleshooting Relay.

--- cookbook/cookbook/settings.py
+++ cookbook/cookbook/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
+# Install the ingredients app
+    'cookbook.ingredients',
+    'cookbook.ingredients.apps',
+    # 'cookbook.recipes.apps.RecipesConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+GRAPHENE = {
+    'SCHEMA': 'cookbook.schema.schema',
+}
